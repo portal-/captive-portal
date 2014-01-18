84fd92229f1a80ed2ff9d05750cc50270b876199
Gautam-Toraskar <gtmtoraskar10@gmail.com>
2014-01-18 13:21:17 -0800

    18-1-2014: 2:14pm -> editing createUser.php

--- createUser.php
+++ createUser.php
@@ -1,10 +1,21 @@
+<!DOCTYPE html>
 <html>
-	<head>
-
-	</head>
-	<body>
-		<form method="POST" acation="userlogin.php">
-				
-		</form>
-	</body>
+  <head>
+    <title>Sign up</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="content/themes/assets/img/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  </head>
+  <body>
+    <div class="loginForm">
+      <center>
+        <form class="form" method="POST" action="authenticate.php">
+          <input class="inputBox" placeholder="guest name" type="text" required/><br/>
+          <input class="button"  type="submit"/> 
+        </form>
+      </center>
+    </div>
+  </body>
 </html>
