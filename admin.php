dc1b0f6c1b6a8917d6f626bd22e0a5de6ab159e8
Gautam-Toraskar <gtmtoraskar10@gmail.com>
2014-01-18 13:21:17 -0800

    first commit

---
+++ admin.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Admin Panel</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+  </head>
+  <body>
+    <center>
+
+      
+      <div>
+        <h1>Admin Panel</h1>
+      </div>
+      <nav class="navbar">
+        <a href="">Create guest</a>
+        <a href="">delete guest</a>
+        <a href="">Logout</a>
+      </nav>
+      <div class="admin-table-div">
+        <table class="user-log">
+          <thead>
+            <tr>
+              <th>Sr no.</th>
+              <th>Guest name</th>
+              <th>Room no.</th>
+              <th>check-in date</th>
+              <th>check-out date</th>
+              <th>OTP</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Kunal Kerkar</td>
+              <td>4</td>
+              <td>22-05-2014</td>
+              <td>26-05-2014</td>
+              <td>Kunal4</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </center>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login form</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="content/themes/assets/img/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  </head>
+  <body>
+    <div class="loginForm">
+      <center>
+        <form class="form" method="POST" action="authenticate.php">
+          <input class="inputBox" placeholder="please enter the PIN" type="text" required/><br/>
+          <input class="button"  type="submit"/> 
+        </form>
+      </center>
+    </div>
+  </body>
+</html>
